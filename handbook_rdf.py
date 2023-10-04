3652f3ae751d7b560a5f388d20bf88112922381d
crystalycteh <crystalhellocrystal@gmail.com>
2023-10-04 15:25:27 +0800

    majors.json read

--- handbook_rdf.py
+++ handbook_rdf.py
@@ -1,3 +1,5 @@
+# Ask about linking to repeated literals like delivery mode and level 
+
 import json
 import re
 from rdflib import FOAF, BNode, Graph, Namespace, Literal, RDF, RDFS
@@ -8,6 +10,10 @@ g = Graph()
 handbook = Namespace("https://handbooks.uwa.edu.au/")
 
 ################# Add initial classes
+# Add general classes
+g.add((handbook["unit"], RDF.type, RDFS.Class))
+g.add((handbook["major"], RDF.type, RDFS.Class))
+
 # Add assessment types as classes
 g.add((handbook["exam"], RDF.type, RDFS.Class))
 g.add((handbook["test"], RDF.type, RDFS.Class))
@@ -29,6 +35,9 @@ g.add((handbook["fieldtrip"], RDF.type, RDFS.Class))
 g.add((handbook["other"], RDF.type, RDFS.Class))
 
 ################# Add initial relations
+# Used in both unit and majors
+
+# Unit relations
 g.add((handbook["level"], RDF.type, RDF.Property)) # connect unit to int unit level
 g.add((handbook["description"], RDF.type, RDF.Property)) # connect unit to literal description
 g.add((handbook["assessment"], RDF.type, RDF.Property)) #connect unit to BN
@@ -37,8 +46,15 @@ g.add((handbook["forHours"], RDF.type, RDF.Property)) #connect BN to int literal
 g.add((handbook["outcome"], RDF.type, RDF.Property)) # connect unit to literal of outcomes
 g.add((handbook["prerequisite_cnf"], RDF.type, RDF.Property)) # connect unit to BN
 g.add((handbook["prerequisite"], RDF.type, RDF.Property)) # connect BN to BN
+g.add((handbook["deliveryMode"], RDF.type, RDF.Property)) 
+
+# Major relations
+g.add((handbook["school"], RDF.type, RDF.Property)) 
+g.add((handbook["course"], RDF.type, RDF.Property))
+g.add((handbook["hasUnit"], RDF.type, RDF.Property)) 
 
 
+# Process unit information
 with open("units.json", "r") as units:
     for unit in json.load(units).values():
         code = handbook[f"unitdetails?code={unit['code']}"]
@@ -46,7 +62,7 @@ with open("units.json", "r") as units:
         g.add((code, RDF.type, handbook["unit"]))
         g.add((code, FOAF.name, Literal(unit["title"].strip())))
         g.add((code, handbook["level"], Literal(int(unit["level"].strip()))))
-
+        g.add((code, handbook["deliveryMode"], Literal(unit["delivery_mode"].strip())))
         g.add((code, handbook["description"], Literal(unit["description"].strip())))
 
         for assessment in unit["assessment"]:
@@ -135,4 +151,26 @@ with open("units.json", "r") as units:
         except KeyError:
             continue
     print()
-    #g.serialize("handbook.xml", "xml")
+
+# Process major information
+with open("majors.json", "r") as majors:
+    for major in json.load(majors).values():
+        code = handbook[f"unitdetails?code={major['code']}"]
+        g.add((code, RDF.type, handbook["major"]))
+
+        g.add((code, FOAF.name, Literal(major["title"].strip())))
+        g.add((code, handbook["school"], Literal(major["school"].strip())))
+        g.add((code, handbook["description"], Literal(major["description"].strip())))
+        
+        for outcome in major["outcomes"]:
+                g.add((code, handbook["outcome"], Literal(outcome.strip())))
+        
+        for course in major["courses"]:
+                g.add((code, handbook["course"], Literal(outcome.strip())))
+                
+        for unit in major["units"]:
+                unit_node = handbook[f"unitdetails?code={unit}"]
+                g.add((code, handbook["hasUnit"], unit_node))
+
+
+
